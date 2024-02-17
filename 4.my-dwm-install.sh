82051a701b9130f82371fc811a2c2dd202705896
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-17 16:47:35 +0100

    Update 4.my-dwm-install.sh

--- 4.my-dwm-install.sh
+++ 4.my-dwm-install.sh
@@ -7,9 +7,9 @@ cd ~/.config/dwm/st && make && sudo make clean install
 
 
 ## Install Suckless slstatus błąd w mojej instalacji
-mkdir ~/.config/dwm/suckles/
-cd ~/.config/dwm/suckles/
-git clone https://git.suckless.org/slstatus
-cd slstatus && make && sudo make clean install
+#mkdir ~/.config/dwm/suckles/
+#cd ~/.config/dwm/suckles/
+#git clone https://git.suckless.org/slstatus
+#cd slstatus && make && sudo make clean install
 
 exit 0
