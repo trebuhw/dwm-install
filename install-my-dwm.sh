53b6036e1e773a2786913ca401530e2997e4bae0
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-17 14:11:10 +0100

    Update install-my-dwm.sh

--- install-my-dwm.sh
+++ install-my-dwm.sh
@@ -1,68 +1,15 @@
 #!/bin/bash
 
-packer="sudo pacman -S --noconfirm --needed"
-#packer="sudo zypper install --no-recommends"
 
-## Xorg & DWM
+cd ~/dwm-install/files/
 
-### Programs
+cp .vimrc .xinitrc .Xresorces ~/
 
-$packer xorg-server
-$packer xorg-xinit
-$packer xorg-xrandr
-$packer xorg-xsetroot
-$packer xdg-user-dirs
-$packer bash-completion
-$packer kitty
-$packer thunar
-$packer firefox
-$packer git
-$packer vim
-$packer feh
-$packer dunst
-$packer btop
-$packer fish
-$packer starship
-$packer flameshot
-$packer lsd
-$packer neofetch
-$packer fastfetch
-$packer neovim
-$packer pdfarranger
-$packer ranger
-$packer rclone
-$packer rsync
-$packer tldr
-$packer zathura
-$packer sxiv
-$packer rofi
-$packer i3lock
-$packer i3color
-$packer gcolor2
-$packer brigtnessctl
-$packer lm-sensors
-$packer polkit
-$packer polkit-gnome
-$packer cups
-$packer sublime-text
-$packer libreoffice
-$packer libreoffice-l10n-pl
+cp ~/dwm-install/files/.config/* ~/.config
 
-cd ~/Pobrane && mkdir -p Suckless && cd Suckless
-
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/st
-git clone https://git.suckless.org/dmenu
-git clone https://git.suckless.org/slstatus
-
-cd dwm && make && sudo make clean install && cd ..
-cd st && make && sudo make clean install && cd ..
-cd dmenu && make && sudo make clean install && cd ..
-cd slstatus && make && sudo make clean install && cd ..
-
+cd ~/.config/dwm && make && sudo make clean install
+cd ~/.config/dwm/dmenu && make && sudo make clean install
+cd ~/.config/dwm/slstatus && make && sudo make clean install
+cd ~/.config/dwm/st && make && sudo make clean install
 
 exit 0
-
-
-
-
