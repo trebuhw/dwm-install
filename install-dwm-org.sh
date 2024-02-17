47afaf5804a3befeac2e42fbe258c45ece724bb8
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-17 10:31:43 +0100

    Update install-dwm-org.sh

--- install-dwm-org.sh
+++ install-dwm-org.sh
@@ -55,6 +55,12 @@ git clone https://git.suckless.org/st
 git clone https://git.suckless.org/dmenu
 git clone https://git.suckless.org/slstatus
 
+cd dwm && make && sudo make clean install && cd ..
+cd st && make && sudo make clean install && cd ..
+cd dmenu && make && sudo make clean install && cd ..
+cd slstatus && make && sudo make clean install && cd ..
+
+
 exit 0
 
 
