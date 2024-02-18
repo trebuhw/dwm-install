a708abbe05e7557d4a46ded8a3726bf491fe9447
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-18 16:11:40 +0100

    Update 09.install-virtualbox.sh

--- install-scripts/09.install-virtualbox.sh
+++ install-scripts/09.install-virtualbox.sh
@@ -7,6 +7,18 @@ echo "##########################################################################
 
 sudo pacman -S --noconfirm --needed virtualbox
 sudo pacman -S --needed --noconfirm virtualbox-host-dkms
+sudo usermod -aG vboxusers $USER
+sudo modprobe vboxdrv
+
+# vboxmanage -v | cut -dr -f1 # sprawdzenie vbox 7.02.12
+
+#Pobrać odpowiedni guest extention pack
+
+# wget https://download.virtualbox.org/virtualbox/7.0.12/Oracle_VM_VirtualBox_Extension_Pack-7.0.12.vbox-extpack
+
+# sudo vboxmanage extpack install Oracle_VM_VirtualBox_Extension_Pack-7.0.12.vbox-extpack
+
+#vboxmanage list extpacks
 
 echo "###########################################################################"
 echo "##      Removing all the messages virtualbox produces         ##"
