9510ea68b3ff5357b7c442f7f627224c4479b639
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-24 00:18:23 +0100

    Create 15.OpenSuse-install-GitHub-Desktop.sh

---
+++ install-scripts/15.OpenSuse-install-GitHub-Desktop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install GitHub Desktop
+
+sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+
+sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/zypp/repos.d/shiftkey-packages.repo'
+
+sudo sh -c 'echo -e "[mwt-packages]\nname=GitHub Desktop\nbaseurl=https://mirror.mwt.me/shiftkey-desktop/rpm\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://mirror.mwt.me/shiftkey-desktop/gpgkey" > /etc/zypp/repos.d/mwt-packages.repo'
+
+sudo zypper refresh && sudo zypper install github-desktop
