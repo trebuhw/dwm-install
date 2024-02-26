26f18b7c8f7af62839229ae0a220b661e9bcc927
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-02-26 20:33:11 +0100

    dwm float rules

--- files/.config/dwm/config.def.h
+++ files/.config/dwm/config.def.h
@@ -49,9 +49,9 @@ static const Rule rules[] = {
 	 */
 	/* class      	instance    title       tags mask     isfloating   monitor    float x,y,w,h         floatborderpx*/
 	{ "Gimp",     	NULL,       NULL,       0,            1,           -1,        50,50,500,500,        -1 },
-	{ "St",         NULL,       NULL,       0,            1,           -1, 				50,50,950,700, 				-1 },
-	{ "Alacritty",  NULL,       NULL,       0,            1,           -1, 				50,50,700,900, 				-1 },
-	{ "Galculator", NULL,       NULL,       0,            1,           -1,        50,50, 10, 10,        -1 },
+	{ "St",         NULL,       NULL,       0,            1,           -1, 				485,190,950,700, 			-1 },
+	{ "Alacritty",  NULL,       NULL,       0,            1,           -1, 				11,33,650,900, 				-1 },
+	{ "Galculator", NULL,       NULL,       0,            1,           -1,        1550,730, 10, 10,     -1 },
 	{ "Firefox",  	NULL,       NULL,       0,            0,           -1,        50,50,500,500,        -1 },
  };
 /* layout(s) */
